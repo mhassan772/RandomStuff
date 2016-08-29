107a3759cca9f24f985b8f3c3ceb5f3616cfc20e
Mohamed M. Hassan <mohamed.magdy.hassan@gmail.com>
2016-08-29 06:29:35 +0300

    Update hash_parser.rb

--- hash_parser.rb
+++ hash_parser.rb
@@ -1,13 +1,38 @@
 #!/usr/bin/env ruby
 #Created by mhassan772
 #Usage: ./hash_parser.rb filename.txt
+###############################################################
+#Sample input:
+#description : AAAA
+#info : BBBB
+#info : CCCC
+#info : DDDD
+#solution : EEEE
+#solution : FFFF
+#reference : GGGG
+#reference : HHHH
+#see_also : IIII
+#see_also : JJJJ
+#http://hi.com : OOOO
+#PPPP : http://hi.com
+##############################################################
+#sample output:
+#{"description"=>["AAAA"], "info"=>["BBBB", "CCCC", "DDDD"], 
+#"solution"=>["EEEE", "FFFF"], "reference"=>["GGGG", "HHHH"], 
+#"see_also"=>["IIII", "JJJJ"], "http://hi.com"=>["OOOO"], "PPPP"=>["http://hi.com"]}
+##############################################################
+#The only condetion on the input for the code to work porbarly is there has to be at least 
+#single space between the ":" and the second value
+#
+#
+#
 
 str = File.read(ARGV[0])
 
 my_hash = {}
 
 str.each_line do |line|
-  first, second = line.split(':')
+  first, second = line.split(': ', 2)
   first.strip!
   second.strip!
  
