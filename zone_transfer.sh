ad7ed3b2de63a7a4bac022e977780384c5f2ff22
Mohamed M. Hassan <mohamed.magdy.hassan@gmail.com>
2016-10-05 06:33:16 +0300

    Update zone_transfer.sh

--- zone_transfer.sh
+++ zone_transfer.sh
@@ -3,9 +3,13 @@
 #Created by mhassan772
 #Usage: ./zone_transfer domain.com
 
+#Get the DNS server using whois
+#$1 = the domain
 DNS=`whois $1 | grep "Name Servers:" -A 1 | xargs -L2 | cut -d' ' -f3`
 
 
 echo "The DNS the script will use is: "$DNS
 echo "Trying Zone Transfer.."
+
+#trying zone transfer using dig
 dig axfr @$DNS $1
