2cab8275cf0448d19788fd8b29b14c9940693d1a
Mohamed M. Hassan <mohamed.magdy.hassan@gmail.com>
2016-12-28 16:23:05 +0300

    Update STIG_Parser.rb
    enter the file name as an argument

--- STIG_Parser.rb
+++ STIG_Parser.rb
@@ -4,34 +4,47 @@
 #This parser will get you the title and how to check the finding. You may uncomment the fix part to get the fix too.
 #You wil need to install nokogiri gem | sudo gem install nokogiri
 
-#file_location = ARGV[0]
+def usage()
+  puts "Usage is:"
+  puts "#{__FILE__} path/to/STIG/file.xml"
+  puts
+end
+
+file_location = ARGV[0]
+
+
+if ARGV.empty?
+  usage
+  exit
+end
 
 require 'nokogiri'
 
-f = File.read('/path/to/file.xml')
+f = File.read(file_location)
 doc = Nokogiri::XML(f)
 
 groups_xml = 
 doc.css('Group').each do |node|
-	title = "#{node.css('Rule').css('title')}"
-	title.slice! "<title>"
-	title.slice! "</title>"
-	puts "The title is: "
-	puts title
+  title = "#{node.css('Rule').css('title')}"
+  title.slice! "<title>"
+  title.slice! "</title>"
+  puts "The title is: "
+  puts title
 
-	check = "#{node.css('Rule').css('check').css('check-content')}"
-	check.slice! "</check-content>"
-	check.slice! ("<check-content>")
-	puts "How to check: "
-	puts check
+  check = "#{node.css('Rule').css('check').css('check-content')}"
+  check.slice! "</check-content>"
+  check.slice! ("<check-content>")
+  puts "How to check: "
+  puts check
 
-	#fix = "#{node.css('Rule').css('fixtext')}"
-	#fix.slice! "</fixtext>"
-	#fix.slice! (/<fixtext fixref=\"F-.*_fix\">/)
-	#puts "The fix is: "
-	#puts fix
+  #fix = "#{node.css('Rule').css('fixtext')}"
+  #fix.slice! "</fixtext>"
+  #fix.slice! (/<fixtext fixref=\"F-.*_fix\">/)
+  #puts "The fix is: "
+  #puts fix
 
 
-	puts "######################################"
-	puts "######################################"
+  puts "######################################"
+  puts "######################################"
 end
+
